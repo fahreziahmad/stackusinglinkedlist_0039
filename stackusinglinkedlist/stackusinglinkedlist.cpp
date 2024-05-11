f64620e7d0d0ad79c3c5f71952766e277f19b4b8
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-05-11 11:55:08 +0700

    add swicth push the entered value onto thhe stack

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -65,7 +65,23 @@ int main() {
 		cout << "4. Exit\n";
 		cout << "Enter your choice: ";
 		cin >> choice;
+		
+		switch (choice) {
+		case 1:
+			cout << "Enter the value to push: ";
+			cin >> value;
+			stack.push(value); // Push the entered value onto the stack
+			break;
+		case 2:
+			if (!stack.isEmpty()) {
+				stack.peek(); // Get the value of the top element
+			}
+			else {
+				cout << "Stack is empty. No top value." << endl;
+			}
+			break;
 
+	
 
 
 
